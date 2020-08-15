9caac2789ff197c78650c7c518502fa3a7296e60
JunoNgx <juno.ngx@gmail.com>
2020-08-15 14:34:33 +0800

    Implement page transitional animation

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,24 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Head from "next/head";
+// import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { PageTransition } from "next-page-transitions"
+import { useRouter } from "next/router";
 
-const Layout = (props) => (
-    <div>
-        <Head>
-            <title>Ebony Memo</title>
-        </Head>
-        <Navbar/>
-        {props.children}
-        <Footer/>
-    </div>
-)
-
-export default Layout
+export default function Layout ({children}) {
+    const router = useRouter
+    return (
+        <div>
+            <Head>
+                <title>Ebony Memo</title>
+            </Head>
+            <Navbar/>
+            {/* <TransitionGroup> */}
+                <PageTransition key={router.pathname} timeout={1000}  classNames="page-transition">
+                    {children}
+                </PageTransition>
+            {/* </TransitionGroup> */}
+            <Footer/>
+        </div>
+    )
+}
