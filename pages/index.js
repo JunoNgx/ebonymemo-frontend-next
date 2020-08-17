b8619fe8570f89f542e8f40cfb2a546b0368ea0c
JunoNgx <juno.ngx@gmail.com>
2020-08-17 14:48:43 +0800

    Tweak landing page random feature rendering

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 import { PageTransition } from "next-page-transitions"
 
 import Layout from '../components/Layout'
@@ -9,9 +10,11 @@ const PAGE_TITLE = "Ebony Memo | Welcome"
 
 export default function Home ({featuredGames}) {
 
+    const [randomIndex, setRandomIndex] = useState(undefined)
+
     // const randomIndex = Math.floor(Math.random() * featuredGames.length)
     // const randomFeaturedGame = featuredGames[randomIndex]
-    const randomFeaturedGame = featuredGames[Math.floor(Math.random() * featuredGames.length)]
+    // const randomFeaturedGame = featuredGames[Math.floor(Math.random() * featuredGames.length)]
 
     // const keys = Object.keys(featuredGames)
     // const randomIndex = Math.floor(Math.random() * keys.length)
@@ -20,14 +23,22 @@ export default function Home ({featuredGames}) {
     // console.log(randomFeaturedGame)
     // const featuredGames = games.filter(game => game.featured)
     // console.log(featuredGames)
+    useEffect(()=>{
+        setRandomIndex(Math.floor(Math.random() * featuredGames.length))
+    }, [featuredGames])
+
+    console.log(randomIndex)
+    // console.log(featuredGames[randomIndex].name)
+
     return (
         <Layout>
             <Head>
                 <title>{PAGE_TITLE}</title>
                 <meta property="og:title" content={PAGE_TITLE} key="title"/>
             </Head>
-            <PageTransition timeout={500} classNames="page-transition">
-                <>
+            <PageTransition key="page-trans" timeout={500} classNames="page-transition">
+                {/* Workaround to probably a bug in next-page-transitions*/}
+                <div key="transit-area">
                     <div className="landing-page">
                         <div className="landing-page__intro">
                             <h1>Videogames are art.</h1>
@@ -38,12 +49,19 @@ export default function Home ({featuredGames}) {
                                 <p className="landing-page__intro__cta-block__cta"><Link href="/browse"><a>Browse the collection</a></Link></p>
                             </div>
                         </div>
-                        <div className="landing-page__feature">
-                            <div className="landing-page__feature__title"><h2>Random feature:</h2></div>
-                            <GameCard game={randomFeaturedGame}/>
-                        </div>
+                        {(randomIndex !== undefined)
+                            ? (
+                                <div className="landing-page__feature">
+                                    <div className="landing-page__feature__title">
+                                        <h2>Random feature:</h2>
+                                    </div>
+                                    <GameCard game={featuredGames[randomIndex]}/>
+                                </div>
+                                )
+                            : ''
+                        }
                     </div>
-                </>
+                </div>
             </PageTransition>
         </Layout>
     )
