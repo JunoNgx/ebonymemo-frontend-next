eb303476ccae436ce738f99699de6c9f2bead96b
JunoNgx <juno.ngx@gmail.com>
2020-08-27 03:07:17 +0800

    Add featured badge and refactor card css

--- components/GameCard.js
+++ components/GameCard.js
@@ -6,36 +6,42 @@ export default function GameCard({game}) {
 
     if (game.android) {
         android = (game.android === "delisted")
-            ? <p className="browse-page__cards__card__footer__links__link browse-page__cards__card__footer__links__link--delisted" title="This Android release of this game has unfortunately been delisted">And</p>
-            : <p className="browse-page__cards__card__footer__links__link" title="Click to access the Android release of this game on the Google Play Store"><a href={game.android} target="_blank">And</a></p>
+            ? <p className="card__footer__links__link card__footer__links__link--delisted" title="This Android release of this game has unfortunately been delisted">And</p>
+            : <p className="card__footer__links__link" title="Click to access the Android release of this game on the Google Play Store"><a href={game.android} target="_blank">And</a></p>
     } else {
         android = ''
     }
 
     if (game.ios) {
         ios = (game.ios === "delisted")
-            ? <p className="browse-page__cards__card__footer__links__link browse-page__cards__card__footer__links__link--delisted" title="The iOS release of this game has unfortunately been delisted">iOS</p>
-            : <p className="browse-page__cards__card__footer__links__link" title="Click to access the iOS release of this game on the Apple App Store"><a href={game.ios} target="_blank">iOS</a></p>
+            ? <p className="card__footer__links__link card__footer__links__link--delisted" title="The iOS release of this game has unfortunately been delisted">iOS</p>
+            : <p className="card__footer__links__link" title="Click to access the iOS release of this game on the Apple App Store"><a href={game.ios} target="_blank">iOS</a></p>
     } else {
         ios = ''
     }
 
     other = (game.other)
-        ? <p className="browse-page__cards__card__footer__links__link" title="This game has at least one release outside of Apple App Store and Google Play Store">oth</p>
+        ? <p className="card__footer__links__link" title="This game has at least one release outside of Apple App Store and Google Play Store">oth</p>
         : ''
 
     return (
-        <div className="browse-page__cards__card">
-            <img className="browse-page__cards__card__cover" src={game.coverUrl}/>
-            <p className="browse-page__cards__card__title">{game.name}</p>
-            <p className="browse-page__cards__card__subtitle">{game.releaseYear} {game.developer.name}</p>
-            <div className="browse-page__cards__card__footer">
-                <div className="browse-page__cards__card__footer__links">
+        <div className="card">
+            <div className="card__cover">
+                {(game.featured)
+                    ? <div className="card__cover__featured-badge" title="This game is currently featured as an Editor's Choice"></div>
+                    : ''
+                }
+                <img className="card__cover__img" src={game.coverUrl}/>
+            </div>
+            <p className="card__title">{game.name}</p>
+            <p className="card__subtitle">{game.releaseYear} {game.developer.name}</p>
+            <div className="card__footer">
+                <div className="card__footer__links">
                     {android}
                     {ios}
                     {other}
                 </div>
-                <p className="browse-page__cards__card__footer__cta"><Link href={`/browse/${game.gameId}`}><a>Read more</a></Link></p>
+                <p className="card__footer__cta"><Link href={`/browse/${game.gameId}`}><a>Read more</a></Link></p>
             </div>
         </div>
     )
