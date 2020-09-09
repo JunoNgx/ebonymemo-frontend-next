51908678d39f33a97174c3f34162b79ef2f51290
JunoNgx <juno.ngx@gmail.com>
2020-09-10 01:00:08 +0800

    Optimize for Lighthouse Audit: add noopener to footer and add service worker

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => (
     <div className="footer">
-        <p className="footer__content">twitter: <a className="footer__content--anchor" href={"https://twitter.com/EbonyMemoCurate"} target={"_blank"}>@EbonyMemoCurate</a><br/>made with love by <a className="footer__content--anchor" href={"http://junongx.com/"} target={"_blank"}>Juno Nguyen</a></p>
+        <p className="footer__content">twitter: <a className="footer__content--anchor" href={"https://twitter.com/EbonyMemoCurate"} rel="noopener" target={"_blank"}>@EbonyMemoCurate</a><br/>made with love by <a className="footer__content--anchor" href={"http://junongx.com/"} rel="noopener" target={"_blank"}>Juno Nguyen</a></p>
     </div>
 )
 
